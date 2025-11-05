d729ea91335836664347d1eb1561d668c75801bb
StyvennDevilliers <styvenn.devilliers@lyceeastier.com>
2025-11-05 20:20:11 +0100

    Affichage des temperatures

--- src/main/java/fr/btsciel/Ihm.java
+++ src/main/java/fr/btsciel/Ihm.java
@@ -1,18 +1,24 @@
 package fr.btsciel;
 
+import java.text.DecimalFormat;
 import java.util.Random;
 
 public class Ihm {
     public static void main(String[] args) {
         Temperature [] temperatures = new Temperature [7];
         String [] jour =   {"lundi","mardi","mercredi","jeudi","vendredi","samedi","dimanche"};
+        DecimalFormat df = new DecimalFormat("0.00");
         Random rand = new Random();
         double valeurmatin,valeursoir;
+        System.out.println("Jour\tMatin\tSoir");
         for (int i = 0; i < temperatures.length; i++) {
              valeurmatin = rand.nextDouble(-10,10) ;
              valeursoir = rand.nextDouble(10,30) ;
              temperatures[i] = new Temperature(jour[i],valeurmatin,valeursoir);
-            System.out.println(temperatures[i].getJour());
+            System.out.print(temperatures[i].getJour() + "\t");
+            System.out.print(df.format(temperatures[i].getMatin()) + "\t");
+            System.out.println(df.format(temperatures[i].getSoir()) + "\t");
+
         }
     }
 }
