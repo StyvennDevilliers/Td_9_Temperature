f306087b735fce9366a1c3aeebefa0bbba02cb95
Fabrice Foray <fabrice@xsharp.eu>
2025-11-14 16:21:19 +0100

    Modifications

--- src/main/java/fr/btsciel/Ihm.java
+++ src/main/java/fr/btsciel/Ihm.java
@@ -4,22 +4,23 @@ import java.text.DecimalFormat;
 
 
 public class Ihm {
+
     public static void main(String[] args) {
         int taille_tableau = 7;
         LesTemperatures temperatures = new LesTemperatures(taille_tableau);
         DecimalFormat df = new DecimalFormat("0.00");
 
         System.out.printf("%-10s",Color.getBLACKBG() + "Jour" + Color.getRESET() + "\t");
-        for(Temperature t : temperatures.getTableau()) {
-            System.out.printf("%-10s", t.getJour());
+        for(int i =0; i< temperatures.getLength(); i++) {
+            System.out.printf("%-10s", temperatures.getTemperature(i).getJour());
         }
         System.out.printf("%-10s","\n" + Color.getBLACKBG() + "Matin" + Color.getRESET() + "\t");
-        for(Temperature t : temperatures.getTableau()) {
-            System.out.printf("%-10s",df.format(t.getMatin()));
+        for(int i =0; i< temperatures.getLength(); i++) {
+            System.out.printf("%-10s",df.format(temperatures.getTemperature(i).getMatin()));
         }
         System.out.printf("%-10s","\n" + Color.getBLACKBG() + "Soir" + Color.getRESET() + "\t");
-        for(Temperature t : temperatures.getTableau()) {
-            System.out.printf("%-10s",df.format(t.getSoir()));
+        for(int i =0; i< temperatures.getLength(); i++) {
+            System.out.printf("%-10s",df.format(temperatures.getTemperature(i).getSoir()));
         }
 
 
--- src/main/java/fr/btsciel/LesTemperatures.java
+++ src/main/java/fr/btsciel/LesTemperatures.java
@@ -8,77 +8,95 @@ import static java.time.format.TextStyle.FULL;
 public class LesTemperatures {
 
     private Temperature [] tableau;
+    private double minMatin;
+    private double maxMatin;
+    private double minSoir;
+    private double maxSoir;
 
     public LesTemperatures(int taille) {
+        minMatin = Double.MAX_VALUE;
+        minSoir = Double.MAX_VALUE;
+        maxMatin = Double.MIN_VALUE;
+        maxSoir = Double.MIN_VALUE;
+        //
         tableau = new Temperature[taille];
         setTableau(taille);
     }
-    public Temperature[] getTableau() {
-        return tableau;
+
+//    public Temperature[] getTableau() {
+//        return tableau;
+//    }
+
+
+    public int getLength() {
+        return tableau.length;
     }
-    public void setTableau(int taille) {
+
+    public Temperature getTemperature(int position){
+        if ( position >= 0 && position < tableau.length ) {
+            Temperature t = tableau[position];
+            // On ne renvoit pas l'objet/la référence originale
+            return new Temperature(t.getJour(),t.getMatin(),t.getSoir());
+        }
+        //Ici, il faudrait générer une exception
+        return null;
+    }
+    public void setTemperature(int position, Temperature t){
+        if ( position >= 0 && position < tableau.length ) {
+            tableau[position] = t;
+            //
+            if (t.getMatin() < minMatin) {
+                minMatin = t.getMatin();
+            }
+            if (t.getMatin() > maxMatin) {
+                maxMatin = t.getMatin();
+            }
+            //
+            if (t.getSoir() < minSoir) {
+                minSoir = t.getSoir();
+            }
+            if (t.getSoir() > maxSoir) {
+                maxSoir = t.getSoir();
+            }
+        }
+        return;
+    }
+
+
+    private void setTableau(int taille) {
         Locale localeFr = Locale.FRENCH;
         Random rand = new Random();
         for (int i = 0; i < taille; i++) {
             LocalDate date = LocalDate.now().minusDays(i);
             double valeurmatin = rand.nextDouble(-20, 5);
             double valeursoir = rand.nextDouble(10, 30);
-            tableau[i] = new Temperature(date.getDayOfWeek().getDisplayName(FULL, localeFr),valeurmatin,valeursoir);
+            //
+            setTemperature(i,new Temperature(date.getDayOfWeek().getDisplayName(FULL, localeFr),valeurmatin,valeursoir));
         }
     }
 
     public double getMaxMatin(){
-        double maxMatin = Double.MIN_VALUE ;
-        for (Temperature t : tableau) {
-            if (t.getMatin() > maxMatin) {
-                maxMatin = t.getMatin();
-            }
-        }
         return maxMatin;
     }
     public double getMinMatin(){
-        double minMatin = Double.MAX_VALUE ;
-        for (Temperature t : tableau) {
-            if (t.getMatin() < minMatin) {
-                minMatin = t.getMatin();
-            }
-        }
         return minMatin;
     }
     public double getMaxSoir(){
-        double maxSoir = Double.MIN_VALUE ;
-        for (Temperature t : tableau) {
-            if (t.getSoir() > maxSoir) {
-                maxSoir = t.getSoir();
-            }
-        }
         return maxSoir;
     }
     public double getMinSoir(){
-        double minSoir = Double.MAX_VALUE ;
-        for (Temperature t : tableau) {
-            if (t.getSoir() < minSoir) {
-                minSoir = t.getSoir();
-            }
-        }
         return minSoir;
     }
     public double getMax(){
-        double max = Double.MIN_VALUE ;
-        for (Temperature t : tableau) {
-            if (t.getSoir() > max) {
-                max = t.getSoir();
-            }
-        }
+        double max = maxMatin ;
+        if ( maxMatin < maxSoir)
+            max = maxSoir;
         return max;
     }
     public double getMin(){
-        double min = Double.MAX_VALUE ;
-        for (Temperature t : tableau) {
-            if (t.getMatin() < min) {
-                min = t.getMatin();
-            }
-        }
+        double min = minMatin ;
+        if ( minMatin < minSoir)
+            min = minSoir;
         return min;
     }
     public double getMoyMatin(int taille){
