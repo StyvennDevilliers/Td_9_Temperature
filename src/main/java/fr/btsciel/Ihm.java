473c60b5ba9ae01733fc95fbd333324ae3c94983
Fabrice Foray <fabrice@xsharp.eu>
2025-11-15 17:51:08 +0100

    Modifications

--- src/main/java/fr/btsciel/Ihm.java
+++ src/main/java/fr/btsciel/Ihm.java
@@ -6,6 +6,52 @@ import java.text.DecimalFormat;
 public class Ihm {
 
     public static void main(String[] args) {
+
+        MonObjet LesObjets_Org[] = new MonObjet[3];
+        for(int i =0; i < LesObjets_Org.length; i++){
+            LesObjets_Org[i] = new MonObjet();
+            LesObjets_Org[i].numero = i;
+            LesObjets_Org[i].nom = "Objet " + (i+1);
+        }
+        for(int i =0; i < LesObjets_Org.length; i++) {
+            System.out.println("Original  : " + LesObjets_Org[i].nom);
+        }
+        // Copie de la référence, donc c'est le même tableau
+        MonObjet LesObjets_Dup[] = LesObjets_Org;
+        // Donc en fait on bosse sur l'original
+        LesObjets_Dup[0].nom = "1111111";
+        for(int i =0; i < LesObjets_Org.length; i++) {
+            System.out.println("Duplicate : " + LesObjets_Dup[i].nom);
+            System.out.println("Original  : " + LesObjets_Org[i].nom);
+        }
+        // Nouvelle référence, mais copie du contenu
+        LesObjets_Dup = new MonObjet[ LesObjets_Org.length];
+        for(int i =0; i < LesObjets_Org.length; i++) {
+            LesObjets_Dup[i] = LesObjets_Org[i];
+        }
+        // Le tableau est nouveau, mais il contient des références vers les objets originaux
+        LesObjets_Dup[1].nom = "2222222";
+        for(int i =0; i < LesObjets_Org.length; i++) {
+            System.out.println("Duplicate : " + LesObjets_Dup[i].nom);
+            System.out.println("Original  : " + LesObjets_Org[i].nom);
+        }
+        // Nouvelle référence, mais copie des caractéristiques
+        LesObjets_Dup = new MonObjet[ LesObjets_Org.length];
+        for(int i =0; i < LesObjets_Org.length; i++) {
+            MonObjet unObjet = new MonObjet();
+            unObjet.numero = LesObjets_Org[i].numero;
+            unObjet.nom = LesObjets_Org[i].nom;
+            LesObjets_Dup[i] = unObjet;
+        }
+        // Le tableau est nouveau, mais il contient des références vers les objets originaux
+        LesObjets_Dup[2].nom = "3333333";
+        for(int i =0; i < LesObjets_Org.length; i++) {
+            System.out.println("Duplicate : " + LesObjets_Dup[i].nom);
+            System.out.println("Original  : " + LesObjets_Org[i].nom);
+        }
+
+
+
         int taille_tableau = 7;
         LesTemperatures temperatures = new LesTemperatures(taille_tableau);
         DecimalFormat df = new DecimalFormat("0.00");
---
+++ src/main/java/fr/btsciel/MonObjet.java
@@ -0,0 +1,7 @@
+package fr.btsciel;
+
+public class MonObjet {
+
+    public int numero;
+    public String nom;
+}
